c06d4abfeedca443c4dbef9394ea131e65653e52
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-23 16:16:00 -0500

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,7 +15,22 @@ const resolvers = {
             
             return await Matchup.find(findQuery);
         }
+    },
+
+    Mutation: {
+        createMatchup: async (parent, args) => {
+            return await Matchup.create(args)    
+        },
+        createVote: async (parent, {_id, techNum}) => {
+            const updatedMatchup = await Matchup.findOneAndUpdate(
+                { _id },
+                { $inc: { [`tech${techNum}_votes`]: 1 } },
+                { new: true }
+              );
+              return updatedMatchup
+        }
     }
+
 }
 
 module.exports = resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,6 +19,11 @@ type Query {
     tech: [Tech]
     matchups(_id: String): [Matchup]
 }
+
+type Mutation {
+    createMatchup(tech1: String!, tech2: String!) : Matchup
+    createVote(_id: String!, techNum: Int!) : Matchup
+}
 `
 
 module.exports = typeDefs
