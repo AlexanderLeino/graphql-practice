3efee8d1e2f4be2df1230594892a92711355fbad
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-23 15:47:26 -0500

    added type defs and able to query data using GraphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,21 @@
-const resolvers = {
+const { Tech, Matchup } = require('../models')
 
+//We need to create queries that use the the entry points that we established in our typeDefs
+
+const resolvers = {
+    Query: {
+        //Tech doesnt have any arguments being passed to it.
+        tech: async () => {
+            return await Tech.find({})
+        },
+        //With matchups we are passing through an options parameter which is _id otherwise we just want to return all matchups
+        matchups: async (parent, { _id }) => {
+            
+            const findQuery = _id ? { _id } : {}
+            
+            return await Matchup.find(findQuery);
+        }
+    }
 }
 
 module.exports = resolvers
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { typeDefs, resolvers } = require('./schemas')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, playground: true, introspection: true })
 
 
 
